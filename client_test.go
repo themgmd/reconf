8b3f0930d419ad40de42fea2a2a59f91853a410a
hasani <mukhammad.me@icloud.com>
2025-11-09 12:04:20 +0300

    fix env; add test

--- client_test.go
+++ client_test.go
@@ -2,6 +2,7 @@ package reconf
 
 import (
 	"context"
+	"github.com/themgmd/reconf/internal/constants"
 	"os"
 	"testing"
 
@@ -62,4 +63,19 @@ func TestClient_NewClient(t *testing.T) {
 	client, err := NewClient()
 	require.NoError(t, err)
 	require.NotNil(t, client)
+
+	val := client.GetValue(context.Background(), "key").String()
+	require.Equal(t, "value", val)
+}
+
+func TestClient_NewClientWithEnv(t *testing.T) {
+	err := os.Setenv(constants.AppEnvKey, constants.EnvDev)
+	require.NoError(t, err)
+
+	client, err := NewClient()
+	require.NoError(t, err)
+	require.NotNil(t, client)
+
+	val := client.GetValue(context.Background(), "http_port").String()
+	require.Equal(t, "1010", val)
 }
